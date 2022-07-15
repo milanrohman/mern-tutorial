8f2e47ac229ade5a5062dd9a085b64ab7d0c0c41
Alma-arif <191240000922@unisnu.ac.id>
2022-07-15 09:08:48 +0700

    middleware endpoint

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,16 @@
+const res = require("express/lib/response")
+
+const erroHandler = (err, req, next) =>{
+    const statusCode = res.statusCode ? res.statusCode : 500
+
+    res.status (statusCode)
+
+    res.json({
+        message: err.message,
+        stack : process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+}
+
+module.exports = {
+    erroHandler
+}
